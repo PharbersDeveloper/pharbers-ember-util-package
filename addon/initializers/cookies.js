5fb840df64829b656fd2d0035bcf7061c87fab45
Faiz <891366402@qq.com>
2018-12-07 14:48:19 +0800

    注入adapter

--- addon/initializers/cookies.js
+++ addon/initializers/cookies.js
@@ -6,6 +6,7 @@ export function initialize(application) {
 	application.inject('route', 'cookie', 'cookie:main');
 	application.inject('controller', 'cookie', 'cookie:main');
 	application.inject('service', 'cookie', 'cookie:main');
+	application.inject('adapter', 'cookie', 'cookie:main');
 }
 
 export default {
